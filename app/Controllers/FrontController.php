4a5ef4bbe915b0cf0d9ca3a30d2c28546a78c367
Jorge Cabrera <126417840+JorgeCabrera2003@users.noreply.github.com>
2026-06-07 12:00:56 -0400

    FIX: Mostrar la vista publica por defecto al iniciar el sistema

--- app/Controllers/FrontController.php
+++ app/Controllers/FrontController.php
@@ -9,6 +9,13 @@
 
         public function __construct() {
 
+            if (!isset($_REQUEST["page"]) || empty($_REQUEST["page"])) {
+                $_REQUEST["page"] = "Noticia";
+                if (!isset($_REQUEST["type"])) {
+                    $_REQUEST["type"] = "publico";
+                }
+            }
+
             if (isset($_REQUEST["page"])) {
 
                 $this->url = $_REQUEST["page"];
