103c77e3a1997337a84806e068fabd907f27215f
lz2712 <leizeraponte2020@gmail.com>
2026-06-08 22:56:46 -0400

    FEAT: Integrando Interfaz Gráfica del Módulo de Tipos de Permisos. se incluye:
    -Vista Principal.
    -Tabla de Datos como partials.
    -Modal Formulario de Tipo de Permisos

---
+++ resources/views/tipo_permiso/partials/_tabla_tipo_permiso.php
@@ -0,0 +1,17 @@
+<!-- ==========================================
+    TABLA DE CATEGORÍA DE INGREDIENTE - REUTILIZABLE
+    ========================================== -->
+
+<div class="table-responsive">
+    <table class="table table-hover align-middle" id="tablaCategoria" style="width:100%">
+        <thead class="table-light">
+            <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- DataTables carga los datos aquí -->
+        </tbody>
+    </table>
+</div>
