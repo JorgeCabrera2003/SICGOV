f603be3e5c735b84e5d9b2a2d215502f515845a9
jacabreram <cabrerajorge2003@gmail.com>
2026-06-30 08:53:02 -0400

    FEAT: archivo backup

--- config/backup.php
+++ config/backup.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * config/backup.php — Generado automáticamente por SICGOV.
- * Última actualización: 2026-06-22 14:53:51
+ * Última actualización: 2026-06-23 10:56:13
  */
 return [
-    'frecuencia'  => 'semanal',
-    'hora'        => 14,
-    'minuto'      => 51,
+    'frecuencia'  => 'diario',
+    'hora'        => 10,
+    'minuto'      => 57,
     'dia_semana'  => 0,
     'dia_mes'     => 1,
-    'actualizado' => '2026-06-22 14:53:51',
+    'actualizado' => '2026-06-23 10:56:13',
 ];
