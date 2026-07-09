8e1d92eb66071d0f7151c47f0d3726af0deb631b
lz2712 <leizeraponte2020@gmail.com>
2026-07-09 00:41:31 -0400

    FIX: Reemplazo de la función confirmarAccion() por MensajeriaHelper.MostrarConfirmacion() en SuministrarInsumoHandler.js.

--- public/assets/js/Handlers/SuministrarInsumoHandler.js
+++ public/assets/js/Handlers/SuministrarInsumoHandler.js
@@ -112,7 +112,7 @@ export async function EnviarDatos(operacion) {
   if (operacion == "suministrar") {
 
     if (Validarenvio()) {
-      confirmacion = await confirmarAccion(`Se va a suministrar un insumo`, mensajeConfirmacion, "question");
+      confirmacion = await MensajeriaHelper.MostrarConfirmacion(`Se va a suministrar un insumo`, mensajeConfirmacion, "question");
 
       if (confirmacion) {
         peticion.append('peticion', "suministrar");
@@ -131,7 +131,7 @@ export async function EnviarDatos(operacion) {
   if (operacion == "suministrar_lote") {
 
     if (ValidadorHelper.ValidarCampo("ID", input.id_insumo, span.id_insumo)) {
-      confirmacion = await confirmarAccion("Se eliminará un Suministrar", mensajeConfirmacion, "warning");
+      confirmacion = await MensajeriaHelper.MostrarConfirmacion("Se eliminará un Suministrar", mensajeConfirmacion, "warning");
 
       if (confirmacion) {
         peticion.append('peticion', 'eliminar');
